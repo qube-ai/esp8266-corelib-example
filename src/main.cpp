1edc61127e2fe3c5133c56d4da6739bfb122f85a
Apoorva Singh <apoorvasingh157@gmail.com>
2021-06-21 01:54:39 +0530

    Divided project in two parts main and init
    Untested commit

--- src/main.cpp
+++ src/main.cpp
@@ -28,29 +28,6 @@ void setup()
   corelib::setup();
   Serial.println("corelib setup complete.");
 
-  // Set WiFi creds
-  storage::setWiFiCreds(0, "Qube", "Deepwaves007");
-  storage::setWiFiCreds(1, "Hrithik", "testing123");
-  storage::setWiFiCreds(2, "", "");
-  storage::setWiFiCreds(3, "", "");
-  storage::setWiFiCreds(4, "", "");
-
-  // Get WiFi Creds
-  storage::wifi_cred cred1 = storage::getWiFiCreds(0);
-  storage::wifi_cred cred2 = storage::getWiFiCreds(1);
-
-  Serial.printf("Credential 1 SSID: %s, Pass: %s\n", cred1.ssid,
-                cred1.password);
-  Serial.printf("Credential 2 SSID: %s, Pass: %s\n", cred2.ssid,
-                cred2.password);
-
-  // Set IoTCore details
-  storage::setProjectID("podnet-switch");
-  storage::setLocation("us-central1");
-  storage::setRegistryID("gen-2");
-  storage::setDeviceID("SU-1P3CH-1");
-  storage::setTimezone(19800);
-
   // Get IoTCore details
   char project_id[20] = "";
   storage::getProjectID(project_id);
